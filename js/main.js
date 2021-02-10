0d1c4b85ff44c0e35ba3f7237dcf4c256f5c3b0b
Lekha <ss.lekha@gmail.com>
2021-02-10 11:17:11 +0200

    subscription message added, parallax started

--- js/main.js
+++ js/main.js
@@ -12,5 +12,30 @@ $('.counter-count').each(function () {
 });
 
 function subscribeFunction() {
-    document.getElementById("thankyou").innerHTML = "Thank you for your subscription."
+    let emailCheck = document.getElementById("email").value;
+    if(emailCheck == "")
+    {
+        document.getElementById("thankyou").style.color = "red";
+        document.getElementById("thankyou").innerHTML = "Please enter email id";
+    }
+    else
+    {
+        document.getElementById("thankyou").style.color = "green";
+        document.getElementById("thankyou").innerHTML = "Thank you for your subscription."
+    }
+    
 }
+
+/*const rss_url = 'http://newsapi.org/v2/top-headlines?' +
+'country=us&' +'apiKey=fc735e5e3785492881825084bef050b3';
+fetch(rss_url,{credentials:'include'})
+.then(response => response.text())
+.then(str => console.log(str));*/
+/*let url = 'http://newsapi.org/v2/top-headlines?' +
+          'country=us&' +
+          'apiKey=fc735e5e3785492881825084bef050b3';
+let req = new Request(url);
+fetch(req)
+    .then(function(response) {
+        console.log(response.json());
+    })*/
